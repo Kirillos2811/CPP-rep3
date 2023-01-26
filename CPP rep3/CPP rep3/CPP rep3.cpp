856b34466957da39b2bb0e55d90e816cb416a669
Kirillos2811 <kirill.sesorov@gmail.com>
2023-01-26 21:29:13 +0300

    Homework 8.5 task 3

--- CPP rep3/CPP rep3/CPP rep3.cpp
+++ CPP rep3/CPP rep3/CPP rep3.cpp
@@ -1,57 +1,147 @@
 ﻿#include <stdio.h>
 #include <stdexcept>
-using namespace std;
 
-class InvalidInstruction : public runtime_error
+class Fraction
 {
+private:
+	float numenator, denumenator;
+
 public:
-	InvalidInstruction() : runtime_error("Invalid instruction was passed") {}
+	Fraction() = default;
+	Fraction(float numenator, float denumenator) {
+		if (denumenator == 0) {
+			throw std::runtime_error("Invalid denumenator: 0");
+		}
+		this->numenator = numenator;
+		this->denumenator = denumenator;
+	}
+
+	float value() const{
+		return numenator / denumenator;
+	}
+
+	void print() const{
+		printf("%f/%f\n", numenator, denumenator);
+	}
+
+	friend Fraction operator + (const Fraction& a, const Fraction& b);
+	friend Fraction operator - (const Fraction& a, const Fraction& b);
+	friend Fraction operator * (const Fraction& a, const Fraction& b);
+	friend Fraction operator / (const Fraction& a, const Fraction& b);
+	friend bool operator == (const Fraction& a, const Fraction& b);
+
+	Fraction operator - () {
+		Fraction res = Fraction(-numenator, denumenator);
+		return res;
+	}
 };
 
-void calc(char str[])
-{	
-	try
-	{
-		int a, b;
-		char operation;
-		float res;
-		sscanf_s(str, "%i%c%i", &a, &operation, (unsigned) sizeof(char), &b);
-		switch (operation)
-		{
-		case '+':
-			res = a + b;
-			break;
-		case '-':
-			res = a - b;
-			break;
-		case '*':
-			res = a * b;
-			break;
-		case '/':
-			res = (float) a / (float) b;
-			break;
-		default:
-			throw InvalidInstruction();
-		}
-		printf("%i %c %i = %f", a, operation, b, res);
+Fraction operator + (const Fraction& a, const Fraction& b) {
+	Fraction res;
+	if (a.denumenator == b.denumenator) {
+		res = Fraction(a.numenator + b.numenator, a.denumenator);
 	}
-	catch (const runtime_error& ex) {
-		throw InvalidInstruction();
+	else {
+		res = Fraction((a.numenator * b.denumenator) + (b.numenator * a.denumenator), a.denumenator * b.denumenator);
 	}
+	return res;
+}
+
+Fraction operator - (const Fraction& a, const Fraction& b) {
+	Fraction res;
+	if (a.denumenator == b.denumenator) {
+		res = Fraction(a.numenator - b.numenator, a.denumenator);
+	}
+	else {
+		res = Fraction((a.numenator * b.denumenator) - (b.numenator * a.denumenator), a.denumenator * b.denumenator);
+	}
+	return res;
+}
+
+Fraction operator * (const Fraction& a, const Fraction& b) {
+	Fraction res = Fraction(a.numenator * b.numenator, a.denumenator * b.denumenator);
+	return res;
+}
+
+Fraction operator / (const Fraction& a, const Fraction& b) {
+	Fraction res = Fraction(a.numenator * b.denumenator, a.denumenator * b.numenator);
+	return res;
+}
+
+bool operator == (const Fraction& a, const Fraction& b) {
+	return a.value() == b.value();
+}
+
+bool operator != (const Fraction& a, const Fraction& b) {
+	return !operator ==(a, b);
+}
+
+bool operator > (const Fraction& a, const Fraction& b) {
+	return a.value() > b.value();
+}
+
+bool operator < (const Fraction& a, const Fraction& b) {
+	return a.value() < b.value();
+}
+
+bool operator >= (const Fraction& a, const Fraction& b) {
+	return a.value() >= b.value();
+}
+
+bool operator <= (const Fraction& a, const Fraction& b) {
+	return a.value() <= b.value();
 }
 
 int main()
 {
-	try
-	{
-		char str[100];
-		scanf_s("%s", str, 100);
-		calc(str);
+	Fraction a = Fraction(3, 2) + Fraction(5, 2);
+	printf("a = ");
+	a.print();
+
+	Fraction b = Fraction(3, 2) - Fraction(5, 2);
+	printf("b = ");
+	b.print();
+
+	Fraction c = Fraction(3, 2) * Fraction(5, 2);
+	printf("c = ");
+	c.print();
+
+	Fraction d = Fraction(3, 2) / Fraction(5, 2);
+	printf("d = ");
+	d.print();
+
+	printf("Fraction(4, 5) == Fraction(20, 25)\n");
+	if (Fraction(4, 5) == Fraction(20, 25)) {
+		printf("true\n");
 	}
-	catch (const InvalidInstruction& ex)
-	{
-		printf("Calculation failed for reason: %s", ex.what());
+	else {
+		printf("false\n");
 	}
+
+	printf("Fraction(4, 5) != Fraction(20, 25)\n");
+	if (Fraction(4, 5) != Fraction(20, 25)) {
+		printf("true\n");
+	}
+	else {
+		printf("false\n");
+	}
+
+	printf("Fraction(4, 5) > Fraction(15, 25)\n");
+	if (Fraction(4, 5) > Fraction(15, 25)) {
+		printf("true\n");
+	}
+	else {
+		printf("false\n");
+	}
+
+	printf("Fraction(4, 5) <= Fraction(20, 25)\n");
+	if (Fraction(4, 5) <= Fraction(20, 25)) {
+		printf("true\n");
+	}
+	else {
+		printf("false\n");
+	}
+
 	return 0;
 }
 
